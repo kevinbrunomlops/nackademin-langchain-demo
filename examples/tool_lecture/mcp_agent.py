0bf10c3d7296cb7205c8158aeec3f1d6c1b61b49
Kevin <kevin.bruno.work@gmail.com>
2026-03-23 16:00:27 +0100

    wrap_tool_call added to the agent

--- examples/tool_lecture/mcp_agent.py
+++ examples/tool_lecture/mcp_agent.py
@@ -1,28 +1,48 @@
 
 import asyncio
+
 from langchain.agents import create_agent
 from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain.agents.middleware import wrap_tool_call
 
 from util.models import get_model
 from util.streaming_utils import STREAM_MODES, handle_stream_async
 from util.pretty_print import print_mcp_tools, get_user_input
 
+@wrap_tool_call
+async def simplify_tool_output(request, handler):
+    result = await handler(request)
+
+    if isinstance(result, dict):
+        if "priority" in result:
+            return (
+                f"Uppgift: {result.get('task_name')}, "
+                f"prioritet: {result.get['priority']}, "
+                f"score: {result.get('score')}"
+            )
+        
+        if "sessions_needed" in result:
+            return (
+                f"Antal studiesessioner som behövs: "
+                f"{result.get('sessions_needed')}"
+            )
+        if "warning" in result:
+            return f"Riskbedömning: {result.get('warning')}"
+    return result 
 
 async def run_async():
     # Get predefined attributes
     model = get_model()
 
     # Create MCP client
-    mcp_client = MultiServerMCPClient({
-        "math_server": {
-            "transport": "streamable_http",
-            "url": "http://localhost:8001/mcp",
-        },
-        "weather": {
-            "transport": "streamable_http",
-            "url": "http://127.0.0.1:8002/mcp"
+    mcp_client = MultiServerMCPClient(
+        {
+            "study_tools": {
+                "transport": "streamable_http",
+                "url": "http://localhost:8003/mcp",
         }
-    })
+    }
+)
 
     # Get tools from MCP client
     tools = await mcp_client.get_tools()
